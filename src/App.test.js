6df26130c3971013288ceae3d70ae98760359843
Heather Hartley <hlhartley@gmail.com>
2019-01-04 15:47:37 -0700

    Adds testing suite enzyme and jest

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,17 @@ import App from './App';
 import { shallow } from 'enzyme';
 
 // reactDOM only on App.test.js
+
+let wrapper;
+describe('App', () => {
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App/>
+    );
+  })
+
+  it('should match the snapshot with all data passed in correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+})
