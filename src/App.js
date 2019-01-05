b4e4473c4523b21fa15c87df49f73534b41f70e5
Heather Hartley <hlhartley@gmail.com>
2019-01-05 01:17:50 -0700

    Makes fetch call for dataset

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import GameContainer from './GameContainer';
 import TerminalCard from './TerminalCard';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+  this.state = {
+    hhTerminalCommands: [],
+  }
+}
+
+  componentDidMount() {
+    debugger
+    fetch("http://memoize-datasets.herokuapp.com/api/v1/hhTerminalCommands")
+      .then(results => results.json())
+      .then((result) => {
+        this.setState({
+          hhTerminalCommands: result.hhTerminalCommands
+        })
+      })
+      .catch(err => {
+        this.setState({error: err})
+      })
+  }
 
   render() {
     return (
