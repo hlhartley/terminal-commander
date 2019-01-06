6053a54d562ad6d5e93170f274392360aada9a30
Heather Hartley <hlhartley@gmail.com>
2019-01-06 01:34:21 -0700

    Adds CSS cloud animation

--- src/GameContainer.js
+++ src/GameContainer.js
@@ -5,8 +5,13 @@ import ProgressBar from './ProgressBar';
 const GameContainer = (props) => {
     return (
         <div className='main-screen'>
-           <h1>TERMINAL COMMANDER</h1>
-           <ProgressBar />
+        <h1>TERMINAL COMMANDER</h1>
+        <div className="cloud x1"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+        <ProgressBar />
         </div>
         )
 }
--- src/TerminalCard.js
+++ src/TerminalCard.js
@@ -44,7 +44,7 @@ export default class TerminalCard extends Component {
   }
 
   clearInput = () => {
-    // document.getElementById("commandInput").reset();
+    document.getElementById("commandInput").value = '';
     this.setState({
       inputValue: ''
     })
