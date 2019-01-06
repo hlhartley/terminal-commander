6396952f21e512212d089478d27c7b3fe2024aef
Heather Hartley <hlhartley@gmail.com>
2019-01-05 22:56:45 -0700

    Adds alt to tank and star images

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -6,8 +6,8 @@ export default class ProgressBar extends Component {
   render() {
     return (
       <div className='progress-container'>
-          <img src='/images/armytank.png' className='tank-img'></img>
-          <img src='/images/star.png' className='star-img pulse'></img>
+          <img src='/images/armytank.png' className='tank-img' alt='tank'></img>
+          <img src='/images/star.png' className='star-img pulse' alt='yellow star'></img>
       </div>
     );
   }
