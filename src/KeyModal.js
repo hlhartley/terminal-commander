c6a8e332b2f7c42964f26386a4217f1a36676734
Heather Hartley <hlhartley@gmail.com>
2019-01-08 15:30:31 -0700

    Makes font awesome icons the same for key and allcommands modal

--- src/KeyModal.js
+++ src/KeyModal.js
@@ -9,9 +9,9 @@ const KeyModal = (props) => {
          </div>
          <h2 className='modal-title'><i class="fas fa-key"></i>Key:</h2>
             <ul className='modal-list fa-ul'>
-                <li><i class="fa-li fas fa-arrow-right"></i><span className='skyblue'>[file]</span> = file name</li>
-                <li><i class="fa-li fas fa-arrow-right"></i><span className='skyblue'>[directory]</span> = directory name</li>
-                <li><i class="fa-li fas fa-arrow-right"></i><span className='skyblue'>[file extension]</span> = file extension type (ex. .html, .css, .txt)</li>
+                <li><i class="fa-li fas fa-angle-double-right"></i><span className='skyblue'>[file]</span> = file name</li>
+                <li><i class="fa-li fas fa-angle-double-right"></i><span className='skyblue'>[directory]</span> = directory name</li>
+                <li><i class="fa-li fas fa-angle-double-right"></i><span className='skyblue'>[file extension]</span> = file extension type (ex. .html, .css, .txt)</li>
             </ul>
      </div>
     );
